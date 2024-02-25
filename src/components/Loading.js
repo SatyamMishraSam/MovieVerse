35717d44e6e287bbe9b38bb92d5a9928d72ea4c2
satyam <mishrasatyamsam@gmail.com>
2024-02-25 21:02:17 +0530

    finaldone

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="w-full  flex  justify-center items-center bg-black">
+    <div  className="w-full  flex  justify-center items-center bg-black">
     <div className="relative flex space-x-2">
         <div className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"></div>
         <div className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"></div>
